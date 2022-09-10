1c0c208ad3180042205f10bd043488ad02474c35
Malay Patra <107231583+malay77patra@users.noreply.github.com>
2022-09-10 07:11:18 +0000

    index

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const {Client, RemoteAuth } = require('whatsapp-web.js');
 const { MongoStore } = require('wwebjs-mongo');
 const mongoose = require('mongoose');
-const  axios = require('axios');
 const url = process.env.URL;
 const MONGO = process.env.MONGO;
 const qrcode = require('qrcode-terminal');
-const msgStore = require("./chat.json");
 
 mongoose.connect(MONGO).then(() => {
   const store = new MongoStore({ mongoose: mongoose });
@@ -21,40 +19,17 @@ mongoose.connect(MONGO).then(() => {
   client.on('qr', (qr) => {
     console.log("qr received", qr);
     qrcode.generate(qr, {small:true});
-    //axios.post(url, {"data": `${qr}`}).then((res) => {
-      //console.log(res.data);
-    //}).catch((err) => {
-      //console.log(err);
-    //});
+    //axios.po
   });
   client.on('ready', () => {
     console.log("<<:Client Ready:>>");
   });
   client.on('message', async msg => {
     const body = await msg.body;
-    const chat = await msg.getChat();
-    const group = await chat.isGroup;
     const type = await msg.type;
     const hasQT = await msg.hasQuotedMsg;
-    if(body==="sticker" || body==="Sticker"){
-      if(type==="image"){
-        const mediaImg = await msg.downloadMedia();
-        chat.sendMessage(mediaImg, {sendMediaAsSticker: true});
-      }else if(type==="video"){
-        const mediaGif = await msg.downloadMedia();
-        chat.sendMessage(mediaGif, {sendMediaAsSticker: true});
-      }else if(type==="chat" && hasQT){
-        const msgQT = await msg.getQuotedMessage();
-        const typeQT = await msgQT.type;
-        if(typeQT==="image"){
-          const mediaImgQT = await msgQT.downloadMedia();
-          chat.sendMessage(mediaImgQT, {sendMediaAsSticker: true});
-        }else if(typeQT==="video"){
-          msg.reply("send the gif with caption ```sticker```");
-        }
-      }
-    }else if(body==="alive" || body==="Alive"){
-      msg.reply("yes 🐤");
+    if (msg.fromMe) {
+      console.log("message from me: " + msg.body);
     }
   });
   console.log("<<:Cliend Initialized:>>");
